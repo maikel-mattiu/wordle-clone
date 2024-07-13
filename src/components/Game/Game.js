a18c554fd63fa989c59a4e7f360b68d36da260a8
Ikenna Onuoha <59225390+maikel-mattiu@users.noreply.github.com>
2024-07-13 15:23:46 +0100

    Update Game.js

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -13,7 +13,7 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants"
 // Pick a random word on every pageload.
 const answer = sample(WORDS)
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer })
+//console.info({ answer })
 
 function Game() {
 	const [guesses, setGuesses] = React.useState([]) // An array of the user guesses
