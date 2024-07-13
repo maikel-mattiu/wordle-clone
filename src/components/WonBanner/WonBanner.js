c3d862cd76857564744b8f3dd66d6def08fe45fd
maikel-mattiu <maikmatt@outlook.com>
2024-07-13 07:21:47 +0100

    Tweak

--- src/components/WonBanner/WonBanner.js
+++ src/components/WonBanner/WonBanner.js
@@ -6,7 +6,7 @@ function WonBanner({ numOfGuesses }) {
 	return (
 		<Banner status="happy">
 			<p>
-				<strong>Congratulations!</strong>Got it in{""}
+				<strong>Congratulations!</strong>Got it in{" "}
 				<strong>
 					{numOfGuesses === 1 ? "1 guess" : `${numOfGuesses} guesses`}
 				</strong>
